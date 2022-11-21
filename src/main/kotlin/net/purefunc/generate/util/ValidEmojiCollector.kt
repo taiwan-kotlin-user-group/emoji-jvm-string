de62ec17afee981ecc311f6935139a74657b446d
akechetaki10736 <chi972121@gmail.com>
2022-11-21 09:10:48 +0800

    Relpace explicit return with implicit return, make it more "Kotlin"

--- src/main/kotlin/net/purefunc/generate/util/ValidEmojiCollector.kt
+++ src/main/kotlin/net/purefunc/generate/util/ValidEmojiCollector.kt
@@ -1,36 +1,35 @@
 package net.purefunc.generate.util
 
-class ValidEmojiCollector(private val soure: List<String>) {
+class ValidEmojiCollector(private val source: List<String>) {
 
-    fun filter(): List<List<String>> {
-        val allValidEmojiEnum = soure.filter {
-            it.contains("fully-qualified")
-        }.map {
-            it.split(" ")
-        }.map { elements ->
-            // [1F636, 200D, 1F32B, FE0F, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , ;, fully-qualified, , , , , #, 😶‍🌫️, E13.1, face, in, cloud]
+    fun filter() =
+        source
+            .filter {
+                it.contains("fully-qualified")
+            }.map {
+                it.split(" ")
+            }.map { elements ->
+                // [1F636, 200D, 1F32B, FE0F, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , ;, fully-qualified, , , , , #, 😶‍🌫️, E13.1, face, in, cloud]
 
-            val items = mutableListOf<String>()
+                val items = mutableListOf<String>()
 
-            val codePartIdxs = elements.mapIndexed { idx, element -> if (element.isEmpty() || element == ";") idx else -1 }
-            val codeIdx = codePartIdxs.first { idx -> idx != -1 }
+                val codePartIdxs = elements.mapIndexed { idx, element -> if (element.isEmpty() || element == ";") idx else -1 }
+                val codeIdx = codePartIdxs.first { idx -> idx != -1 }
 
-            (0 until codeIdx).forEach { i -> items.add(elements[i]) }
-            items.add(";")
+                (0 until codeIdx).forEach { i -> items.add(elements[i]) }
+                items.add(";")
 
-            val namePartIdxs =
-                elements.mapIndexed { idx, element -> if (element.startsWith("E") && element.contains(".")) idx else -1 }
-            val nameIdx = namePartIdxs.first { idx -> idx != -1 }
+                val namePartIdxs =
+                    elements.mapIndexed { idx, element -> if (element.startsWith("E") && element.contains(".")) idx else -1 }
+                val nameIdx = namePartIdxs.first { idx -> idx != -1 }
 
-            items.plus(elements.subList(nameIdx + 1, elements.size).map { str -> convertToValidChar(str) })
-        }
+                items.plus(elements.subList(nameIdx + 1, elements.size).map { str -> convertToValidChar(str) })
+            }
 
-        return allValidEmojiEnum
-    }
 
-    private fun convertToValidChar(str: String): String {
+    private fun convertToValidChar(str: String) =
         // replace item name contains invalid char
-        return str.replace("“", "")
+        str.replace("“", "")
             .replace("”", "")
             .replace("’", "")
             .replace("-", "_")
@@ -47,5 +46,5 @@ class ValidEmojiCollector(private val soure: List<String>) {
             .replace("*", "star")
             .replace(",", "comma")
             .replace("&", "and")
-    }
+
 }
